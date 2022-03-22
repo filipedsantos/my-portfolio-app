d450eb43383225db1c70b7161300a7c37369f292
filipedsantos <fd.filipe@gmail.com>
2022-03-22 12:34:25 +0100

    Add qualification section. Misses final customizations.

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,24 @@ skillsContainer.addEventListener('click', function (e) {
     clicked.classList.remove('skills__close');
   }
 });
+
+// Qualification tabs
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification__active');
+    });
+    target.classList.add('qualification__active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification__active');
+    });
+    tab.classList.add('qualification__active');
+  });
+});
